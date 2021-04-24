a8ad365b645449b5582675a79db3c8669199a101
Janusz <janusz@januszwisniowski.it>
2021-04-24 18:19:44 +0200

    [XPTH-6] [XPTH-46] Cherry-picking changes.

--- src/main/java/org/eclipse/wst/xml/xpath2/processor/internal/utils/ScalarTypePromoter.java
+++ src/main/java/org/eclipse/wst/xml/xpath2/processor/internal/utils/ScalarTypePromoter.java
@@ -14,26 +14,54 @@
 
 package org.eclipse.wst.xml.xpath2.processor.internal.utils;
 
+import org.eclipse.wst.xml.xpath2.processor.DynamicError;
+import org.eclipse.wst.xml.xpath2.processor.internal.types.AnyAtomicType;
+import org.eclipse.wst.xml.xpath2.processor.internal.types.XSDate;
+import org.eclipse.wst.xml.xpath2.processor.internal.types.XSDateTime;
 import org.eclipse.wst.xml.xpath2.processor.internal.types.XSDayTimeDuration;
 import org.eclipse.wst.xml.xpath2.processor.internal.types.XSYearMonthDuration;
 
 public class ScalarTypePromoter extends NumericTypePromoter {
 
-	protected boolean checkCombination(Class newType) {
+    @Override
+    protected boolean checkCombination(Class newType) {
 
-		Class targetType = getTargetType();
-		if (targetType == XSDayTimeDuration.class || targetType == XSYearMonthDuration.class) {
-			return targetType == newType;	
-		}
-		return super.checkCombination(newType);
-	}
+        Class targetType = this.getTargetType();
+        if (targetType == XSDayTimeDuration.class || targetType == XSYearMonthDuration.class) {
+            return targetType == newType;
+        }
+        if (newType == XSDate.class) {
+            return targetType == XSDate.class;
+        }
 
-	protected Class substitute(Class typeToConsider) {
-		if (typeToConsider == XSDayTimeDuration.class || typeToConsider == XSYearMonthDuration.class) {
-			return typeToConsider;
-		}
+        if (newType == XSDateTime.class) {
+            return targetType == XSDateTime.class;
+        }
 
-		return super.substitute(typeToConsider);
-	}
-	
+        return super.checkCombination(newType);
+    }
+
+    @Override
+    public AnyAtomicType doPromote(AnyAtomicType value) throws DynamicError {
+        if (this.getTargetType() == XSDate.class) {
+            return XSDate.parse_date(value.getStringValue());
+        } else if (this.getTargetType() == XSDateTime.class) {
+            return XSDateTime.parseDateTime(value.getStringValue());
+        } else if (this.getTargetType() == XSYearMonthDuration.class) {
+            return XSYearMonthDuration.parseYMDuration(value.getStringValue());
+        } else if (this.getTargetType() == XSDayTimeDuration.class) {
+            return XSDayTimeDuration.parseDTDuration(value.getStringValue());
+        }
+        return super.doPromote(value);
+    }
+
+    @Override
+    protected Class substitute(Class typeToConsider) {
+        if (typeToConsider == XSDayTimeDuration.class || typeToConsider == XSYearMonthDuration.class) {
+            return typeToConsider;
+        }
+
+        return super.substitute(typeToConsider);
+    }
+
 }
